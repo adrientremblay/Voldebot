3ea57022a5dfbc41e300d01486bc3b8e897de4d5
Adrien Tremblay <adrien@adrientremblay.com>
2020-11-13 16:43:31 -0500

    print channels

--- bot.js
+++ bot.js
@@ -5,7 +5,11 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log("Connected as: " + client.user.tag);
-  client.user.setActivity("Studying the Dark Arts");
+  client.user.setActivity("The Dark Arts", { type: "STUDYING" });
+
+  client.guilds.cache.forEach((channel) => {
+    console.log(`${channel.name} ${channel.type} ${channel.id}`);
+  });
 });
 
 client.login(process.env.BOT_TOKEN);
