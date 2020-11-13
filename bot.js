c7277537fc6d2747d62f0d75e26fc276a002c966
Adrien Tremblay <adrien@adrientremblay.com>
2020-11-13 17:15:05 -0500

    command identifier

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const COMMAND_IDENTIFIER = "voldemort ";
+
 const avada_kedavra = new Discord.MessageAttachment(
   "https://thumbs.gfycat.com/AcademicIllustriousGallowaycow-max-1mb.gif"
 );
@@ -23,7 +25,12 @@ client.on("message", (receivedMessage) => {
   if (receivedMessage.author == client.user) {
     return;
   }
-  receivedMessage.channel.send("shut up you PLEB");
+
+  if (receivedMessage.content.startsWith(COMMAND_IDENTIFIER)) {
+    receivedMessage.channel.send(
+      "shut up " + receivedMessage.author.toString()
+    );
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
