38304660fca4d5f8230c6b99dd5b55910d7170c0
Adrien Tremblay <adrien@adrientremblay.com>
2020-11-13 16:45:24 -0500

    fix print channels

--- bot.js
+++ bot.js
@@ -7,8 +7,10 @@ client.on("ready", () => {
   console.log("Connected as: " + client.user.tag);
   client.user.setActivity("The Dark Arts", { type: "STUDYING" });
 
-  client.guilds.cache.forEach((channel) => {
-    console.log(`${channel.name} ${channel.type} ${channel.id}`);
+  client.guilds.cache.forEach((guild) => {
+    guild.channels.cache.forEach((channel) => {
+      console.log(`${channel.name} ${channel.type} ${channel.id}`);
+    });
   });
 });
 
